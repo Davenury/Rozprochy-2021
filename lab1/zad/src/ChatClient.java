1168779c8521a7f66e6cca865a195b64ab1d7e37
Dawid Dębowski <dawid199960@gmail.com>
2021-03-05 07:59:27 +0100

    Homework 1 UDP works

--- lab1/zad/src/ChatClient.java
+++ lab1/zad/src/ChatClient.java
@@ -2,17 +2,23 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
-import java.net.Socket;
+import java.net.*;
 import java.util.Scanner;
+import java.util.regex.Pattern;
 
 public class ChatClient {
 
+    static int port = 12345;
+    static String name = "localhost";
+
     public static void main(String[] args) throws IOException {
-        Socket socket = establishConnection();
-        if(socket == null){
+        SocketPacket socketPacket = establishConnection();
+        if(socketPacket == null || socketPacket.tcp == null || socketPacket.udp == null){
             System.out.println("Error while establishing the connection!");
             return;
         }
+        Socket socket = socketPacket.tcp;
+        DatagramSocket udpSocket = socketPacket.udp;
         PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
         BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 
@@ -29,35 +35,62 @@ public class ChatClient {
         });
         thread.start();
 
+        Thread udpThread = new Thread(() -> {
+            try{
+                byte[] receiveBuffer = new byte[1024];
+                while(!Thread.interrupted()){
+                    DatagramPacket receivePacket = new DatagramPacket(receiveBuffer, receiveBuffer.length);
+                    udpSocket.receive(receivePacket);
+                    System.out.println(new String(receivePacket.getData()));
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+        udpThread.start();
+
         while(true){
             String command = getCommand();
-            int code = parseCommand(command, socket, out, in, thread);
+            int code = parseCommand(command, socket, out, in, thread, udpSocket, udpThread);
             if(code == -1){
                 break;
             }
         }
     }
 
-    private static Socket establishConnection(){
+    private static void sendByUDP(DatagramSocket socket, String msg) throws IOException {
+        byte[] sendBuffer = msg.getBytes();
+        DatagramPacket sendPacket = new DatagramPacket(sendBuffer,
+                sendBuffer.length, InetAddress.getByName(name), port);
+        socket.send(sendPacket);
+    }
+
+    private static SocketPacket establishConnection(){
         Socket socket;
+        DatagramSocket udpSocket;
         try {
-            socket = new Socket("localhost", 12345);
-            return socket;
+            socket = new Socket(name, port);
+            udpSocket = new DatagramSocket();
+            sendByUDP(udpSocket, "Init");
+            return new SocketPacket(socket, udpSocket);
         } catch (IOException e){
             e.printStackTrace();
             return null;
         }
     }
 
-    private static void closeConnection(Socket socket, Thread thread, PrintWriter out) throws IOException {
+    private static void closeConnection(Socket socket, Thread thread,
+                                        PrintWriter out, DatagramSocket udpSocket, Thread udpThread) throws IOException {
         if(socket != null){
             thread.interrupt();
+            udpThread.interrupt();
             try {
-                Thread.sleep(1000);
+                Thread.sleep(3000);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
             out.println("close");
+            udpSocket.close();
             socket.close();
         }
     }
@@ -68,14 +101,28 @@ public class ChatClient {
     }
 
     private static int parseCommand(String command, Socket socket, PrintWriter out,
-                                    BufferedReader in, Thread thread) throws IOException {
+                                    BufferedReader in,
+                                    Thread thread, DatagramSocket udpSocket, Thread udpThread) throws IOException {
         command = command.toLowerCase();
+        Pattern startsWithU = Pattern.compile("^u");
         if(command.equals("logout")){
-            closeConnection(socket, thread, out);
+            closeConnection(socket, thread, out, udpSocket, udpThread);
             return -1;
+        } else if(startsWithU.matcher(command).find()){
+            sendByUDP(udpSocket, command.substring(2));
         } else{
             out.println(command);
         }
         return 0;
+    }
+
+    private static class SocketPacket{
+        Socket tcp;
+        DatagramSocket udp;
+
+        public SocketPacket(Socket tcpSocket, DatagramSocket udpSocket){
+            tcp = tcpSocket;
+            udp = udpSocket;
+        }
     }
 }
--- lab1/zad/src/Server.java
+++ lab1/zad/src/Server.java
@@ -2,8 +2,8 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
-import java.net.ServerSocket;
-import java.net.Socket;
+import java.net.*;
+import java.nio.charset.Charset;
 import java.util.*;
 import java.util.regex.Pattern;
 
@@ -11,55 +11,114 @@ public class Server {
     public static void main(String[] args) throws IOException {
 
         List<ServerClient> users = new ArrayList<>();
+        List<UDPClient> udpUsers = new ArrayList<>();
 
-        Pattern pattern = Pattern.compile("login [a-z]+");
-
-        ServerSocket serverSocket = createSocket();
-        if(serverSocket == null){
+        SocektsPacket sockets = createSocket();
+        ServerSocket serverSocket;
+        DatagramSocket udpSocket;
+        if(sockets == null || sockets.tcp == null || sockets.udp == null){
             return;
         }
+        serverSocket = sockets.tcp;
+        udpSocket = sockets.udp;
         try{
-            while(true){
-                Socket clientSocket = serverSocket.accept();
-                Thread createUserAndListen = new Thread(() -> {
-                    try {
-                        ServerClient client = new ServerClient(clientSocket);
-                        users.add(client);
-                        while(!Thread.interrupted()){
-                            String msg = client.in.readLine();
-                            if(msg.equals("close")){
-                                client.close();
-                                Thread.currentThread().interrupt();
-                            }else if(pattern.matcher(msg).matches()){
-                                msg = msg.substring(6);
-                                if(checkForUsersWithThisName(users, client, msg)){
-                                    client.sendBackMessage("Username already exists");
-                                }
-                                else {
-                                    System.out.println("here");
-                                    client.setLogin(msg);
-                                    client.sendBackMessage("Successfully logged in!");
-                                }
-                            }
-                            else {
-                                if(client.login == null){
-                                    client.sendBackMessage("You need to login before writing into chat!");
-                                }
-                                else {
-                                    sendMessages(users, msg, client);
-                                }
-                            }
-                        }
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                });
-                createUserAndListen.start();
-            }
+            listenOnUDP(udpSocket, udpUsers);
+            listenOnTCP(serverSocket, users);
         } catch(Exception e){
             e.printStackTrace();
         } finally {
             serverSocket.close();
+        }
+    }
+
+    private static void listenOnUDP(DatagramSocket udpSocket, List<UDPClient> users){
+        Thread thread = new Thread(() -> {
+            try {
+                byte[] receiveBuffer = new byte[1024];
+                while (true) {
+                    DatagramPacket receivePacket = new DatagramPacket(receiveBuffer, receiveBuffer.length);
+                    udpSocket.receive(receivePacket);
+                    String msg = new String(receivePacket.getData(), 0, receivePacket.getLength());
+
+                    InetAddress address = receivePacket.getAddress();
+                    int port = receivePacket.getPort();
+                    UDPClient user = new UDPClient(address, port);
+
+                    if(msg.equals("Init")){
+                        users.add(user);
+                    } else{
+                        for(UDPClient udpUser : users){
+                            if(!udpUser.equals(user))
+                                user.sendMessage(msg, udpSocket);
+                        }
+                    }
+                }
+            } catch(Exception e){
+                e.printStackTrace();
+            } finally {
+                udpSocket.close();
+            }
+        });
+        thread.start();
+    }
+
+    private static void listenOnTCP(ServerSocket serverSocket, List<ServerClient> users)
+            throws IOException {
+        Pattern pattern = Pattern.compile("login [a-z]+");
+        while(true){
+            Socket clientSocket = serverSocket.accept();
+            Thread createUserAndListen = new Thread(() -> {
+                serviceClient(clientSocket, users, pattern);
+            });
+            createUserAndListen.start();
+        }
+    }
+
+    private static void serviceClient(Socket clientSocket, List<ServerClient> users, Pattern pattern){
+        try {
+            ServerClient client = new ServerClient(clientSocket);
+            users.add(client);
+            while(!Thread.interrupted()){
+                parseMessage(users, client, pattern);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void parseMessage(List<ServerClient> users, ServerClient client, Pattern pattern)
+            throws IOException {
+        String msg = client.in.readLine();
+        System.out.println("Sent by TCP: " + msg);
+        if(msg.equals("close")){
+            client.close();
+            users.remove(client);
+            Thread.currentThread().interrupt();
+        }else if(pattern.matcher(msg).matches()){
+            loginTCPClient(users, client, msg);
+        }
+        else {
+            sendMessagesOfLoginClient(users, client, msg);
+        }
+    }
+
+    private static void loginTCPClient(List<ServerClient> users, ServerClient client, String msg){
+        msg = msg.substring(6);
+        if(checkForUsersWithThisName(users, client, msg)){
+            client.sendBackMessage("Username already exists");
+        }
+        else {
+            client.setLogin(msg);
+            client.sendBackMessage("Successfully logged in!");
+        }
+    }
+
+    private static void sendMessagesOfLoginClient(List<ServerClient> users, ServerClient client, String msg){
+        if(client.login == null){
+            client.sendBackMessage("You need to login before writing into chat!");
+        }
+        else {
+            sendMessages(users, msg, client);
         }
     }
 
@@ -82,11 +141,13 @@ public class Server {
         }
     }
 
-    private static ServerSocket createSocket(){
+    private static SocektsPacket createSocket(){
         ServerSocket serverSocket;
+        DatagramSocket udpSocket;
         try{
             serverSocket = new ServerSocket(12345);
-            return serverSocket;
+            udpSocket = new DatagramSocket(12345);
+            return new SocektsPacket(serverSocket, udpSocket);
         } catch(IOException e){
             System.out.println("Error while creating socket!");
             return null;
@@ -129,6 +190,47 @@ public class Server {
             return Objects.equals(socket, that.socket) &&
                     Objects.equals(out, that.out) &&
                     Objects.equals(in, that.in);
+        }
+    }
+
+    private static class SocektsPacket{
+        ServerSocket tcp;
+        DatagramSocket udp;
+        public SocektsPacket(ServerSocket tcpSocket, DatagramSocket udpSocket){
+            tcp = tcpSocket;
+            udp = udpSocket;
+        }
+    }
+
+    private static class UDPClient{
+        InetAddress address;
+        int port;
+        public UDPClient(InetAddress udpAddress, int udpPort){
+            address = udpAddress;
+            port = udpPort;
+        }
+
+        public InetAddress getAddress(){
+            return address;
+        }
+
+        public int getPort(){
+            return port;
+        }
+
+        public void sendMessage(String msg, DatagramSocket socket) throws IOException {
+            byte[] sendBuffer = msg.getBytes();
+            DatagramPacket sendPacket = new DatagramPacket(sendBuffer, sendBuffer.length, getAddress(), getPort());
+            socket.send(sendPacket);
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            UDPClient udpClient = (UDPClient) o;
+            return port == udpClient.port &&
+                    Objects.equals(address, udpClient.address);
         }
     }
 }
